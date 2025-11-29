5b0ae5ece9edd37f7ade690f15e82c5ff41ad36c
Roshan Kumar <isthisroshan@gmail.com>
2025-11-29 11:54:33 +0000

    cleaning files

--- 00livePractice.py
+++ 00livePractice.py
@@ -1,97 +1,10 @@
 import pandas as pd
 
-df = pd.read_csv("messy_restaurant_data.csv")
-# print(df)
+df = pd.read_csv("students_performance_level3.csv")
 # print(df.head())
 # print(df.info())
 # print(df.shape)
-# print(df.isnull())
-# print(df.fillna("Unknown"))
-
-# print(df.isnull().sum())
-# print(df.dropna())
-# print(df.dropna(axis=1))
-# print(df.isnull().sum())
-# df = df.fillna("OKAY")
-# print(df)
-# print(df.duplicated())
-# print(df.duplicated().sum())
-# df = df.drop_duplicates()
-# print(df)
-# df = df.drop_duplicates(subset=["Restaurant_Name"])
-# print(df)
-# df.columns = df.columns.str.replace(" ", "_")
-# print(df.columns)
-
-# df["Rating"] = (
-#     df["Rating"]
-#     .astype(str)
-#     .str.lower()
-#     .str.strip()
-#     .str.extract(r'(\d+\.?\d*)')
-# )
-# df["Rating"] = pd.to_numeric(df["Rating"], errors="coerce")
-# print(df["Rating"])
-
-
-# df["Restaurant_Name"] =(
-#     df["Restaurant_Name"]
-#     .str.lower()
-#     .str.replace(r"\s+", " ", regex=True)
-#     .str.strip()
-#     .str.title()
-
-# )
-# print(df["Restaurant_Name"])
-
-
-# df["City"] = (
-#     df["City"]
-#     .astype(str)
-#     .str.lower()
-#     .str.strip()
-#     .str.replace(r"\s+", " ",regex=True)
-#     .replace({
-#         "banglore" : "bangalore",
-#         "hyderbad" : "hyderabad"
-#     })
-#     .str.title()
-
-# )
-
-# print(df["City"])
-
-# df["Cuisine"] = (
-#     df["Cuisine"]
-#     .astype(str)
-#     .str.lower()
-#     .str.strip()
-#     .str.replace(r"\s+", " ", regex=True)
-#     .str.replace("-", " ")
-#     .replace({
-#         "chineese" : "chinese",
-#         "nan" : pd.NA
-#     })
-#     .str.title()
-
-# )
-# print(df["Cuisine"])
-
-df["Price"] = (
-    df["Price"] 
-    .astype(str)
-    .str.lower()
-    .str.replace(r"[₹$]","", regex=True)
-    .str.replace(r"rs|inr", "", regex=True)
-    .str.replace(r'\s+', " ", regex=True)
-    .str.strip()
-    .replace({
-        "one hundred" : 100,
-        "two hundred" : 200,
-        "three hundred" : 300
-    })
-    .str.extract(r"(\d+\.?\d*)")
-
-)
-df["Price"] = pd.to_numeric(df["Price"], errors="coerce")
-print(df["Price"])
+print(df["full_name"])
+print(df[["full_name"]])
+print(type(df["full_name"]))
+print(type(df[["full_name"]]))
--- in.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("sales_data_sample.csv", encoding="latin1")
-print('Displaying the info of the Datasets')
-
-print(df.info())
